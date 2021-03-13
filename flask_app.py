d0b8bd1441e35e54f64dd8f6543325228bcce974
Dm <YDimitry@mail.ru>
2021-03-13 19:18:01 +0300

    2nd commit

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def hello_world():
     return 'Hello from Flask!'
+
+# if __name__ == "__main":
+#     app.run()
 
